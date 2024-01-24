bdeab1e0b4f47525e9459087ad8637aa6f2695d8
ratchanan_sak <ratchanan.sak@gmail.com>
2024-01-24 11:03:21 +0700

    feat: update to 0.1.1

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  printWidth: 120,
+  semi: false,
+}
